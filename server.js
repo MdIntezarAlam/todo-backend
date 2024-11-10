69a9103708af4b7f60fc7ee27f8708bbca387377
Md Intezar Alam <mdintezara35@gmail.com>
2024-11-10 19:02:10 +0530

    4th

--- server.js
+++ server.js
@@ -60,8 +60,6 @@
 
 // //this is the deplyed link api =>https://dev-intezar-todo.onrender.com/api/v2/fetch
 
-
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -71,26 +69,24 @@ import userRouter from "./src/router/userRouter.js";
 import addressRouter from "./src/router/addressRouter.js";
 import ticketRouter from "./src/router/ticketRouter.js";
 import cookieParser from "cookie-parser";
-import { Server } from "socket.io";
-import { createServer } from "http";
-import { socketConnectionController } from "./src/controller/socketController.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-// Update CORS configuration to allow credentials and specify origin
+app.options( '*', cors() ); // Handle preflight requests globally
+
 const allowedOrigins = [
   "http://localhost:3000", // Development local server
-  "http://localhost:3001", // Another local server if applicable
   "https://dev-intezar-todo.onrender.com", // Production Render URL
   "https://fullstack-forntend.netlify.app" // Production Netlify URL
 ];
 
 const corsOptions = {
   origin: function ( origin, callback ) {
-    if ( allowedOrigins.indexOf( origin ) !== -1 )
+    console.log( "Incoming origin:", origin ); // Debugging line to check incoming origin
+    if ( allowedOrigins.indexOf( origin ) !== -1 || !origin )
     {
       callback( null, true );
     } else
@@ -101,32 +97,16 @@ const corsOptions = {
   credentials: true, // Enable credentials (cookies, authorization headers)
 };
 
-app.options( '*', cors() ); // Handle preflight requests globally
 app.use( cors( corsOptions ) );
-
 app.use( cookieParser() );
 app.use( express.json() );
-
-// Define the socket server after creating the Express server
-const server = createServer( app );
-
-// Initialize the socket.io instance
-const io = new Server( server, {
-  cors: {
-    origin: allowedOrigins,
-    credentials: true,
-  },
-} );
-
-// Now you can pass `io` to your `socketConnectionController`
-socketConnectionController( io );
-
 app.use( "/api/v2", todoRouter );
 app.use( "/api/v2/auth", userRouter );
 app.use( "/api/v2/address", addressRouter );
 app.use( "/api/v2", ticketRouter );
 
-const PORT = process.env.PORT || 5000; // Default to port 5000 if process.env.PORT is undefined
-server.listen( PORT, () => {
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen( PORT, () => {
   console.log( `Server is running on port ${ PORT }` );
 } );
